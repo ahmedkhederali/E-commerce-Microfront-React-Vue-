866f0679b0bf7c7933d354b7207d3391d24d612f
ahmed <ahhmeadali1256@gmail.com>
2025-02-07 17:40:11 +0200

    ..

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -2,7 +2,7 @@ import {mount} from  "marketing/MarketingApp";
 import React ,{useRef, useEffect} from 'react'
 
 // we are make thiscompoant to makeit generic 
-// so u can use the same approach in diffrent framwork
+// so u can use the same approach in diffrent framework
 export default ()=> {
     const refEle=useRef(null);
     useEffect(()=>{
