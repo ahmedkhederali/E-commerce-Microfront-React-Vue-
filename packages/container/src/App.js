d9ebe744b7d8a1eb017ae59d4b3f0f1f19156fe6
ahmed <ahhmeadali1256@gmail.com>
2025-02-11 21:12:59 +0200

    change production prefix so in css-in-js instead of making a conflict make a prefic for each project

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import { StylesProvider , createGenerateClassName } from "@material-ui/core/styles";
+
+const generateClassName= createGenerateClassName({
+  productionPrefix:'co'
+})
 export default function App() {
   return (
+  <StylesProvider generateClassName={generateClassName}>
     <BrowserRouter>
       <div>
         <Header />
         <MarketingApp />
       </div>
     </BrowserRouter>
+    </StylesProvider>
   );
 }
