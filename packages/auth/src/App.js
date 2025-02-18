2940fae8adcdeac7787300b17480e021160d75f5
ahmed <ahhmeadali1256@gmail.com>
2025-02-18 07:47:52 +0200

    solve issue name in local called SignUp and it named is SignUp GitHub Actions uses a Linux environment, which is case-sensitive, unlike macOS and Windows. If your component file is named:Signup

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider , createGenerateClassName } from "@material-ui/core/styles";
 
 
-import SignUp from './components/SignUp';
+import SignUp from './components/Signup';
 import SignIn from './components/Signin';
 
 const generateClassName= createGenerateClassName({
